27a5740bb7b825accff3b68895f0fe8b9009371e
Shadman <shadman.hassan@live.ca>
2019-09-28 15:11:33 -0400

    Added player sprite and player controller script

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D rb;
+
+    bool moveLeft = false;
+    bool moveRight = false;
+    bool moveUp = false;
+    bool moveDown = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            moveLeft = true;
+        }
+        else
+        {
+            moveLeft = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (moveLeft)
+        {
+            rb.AddForce(new Vector2(-5, 0), ForceMode2D.Impulse);
+        }
+    }
+}
