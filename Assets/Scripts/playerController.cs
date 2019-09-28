e8e9a967d2eb50bad490a7ce59390fe264c4c48e
Sajid Rahman <potatorahman@gmail.com>
2019-09-28 17:36:24 -0400

    added grid snapping to player movement

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,21 +7,32 @@ public class playerController : MonoBehaviour
 {
     [SerializeField]
     private Rigidbody2D rb;
+    [SerializeField]
+    private Transform transform;
 
     bool moveLeft = false;
     bool moveRight = false;
     bool moveUp = false;
     bool moveDown = false;
 
+    private bool snapMovement;
+    private bool isStill = true;
+    private int prevX;
+    private int prevY;
+    private int velX = 0;
+    private int velY = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        prevX = (int)transform.position.x;
+        prevY = (int)transform.position.y;
     }
 
     // Update is called once per frame
     private void Update()
     {
+        
         moveLeft = Input.GetAxis("Horizontal") < 0;
         moveRight = Input.GetAxis("Horizontal") > 0;
         moveUp = Input.GetAxis("Vertical") > 0;
@@ -29,21 +41,44 @@ public class playerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (moveLeft)
+        if (isStill)
         {
-            rb.AddForce(new Vector2(-5, 0), ForceMode2D.Impulse);
+            isStill = false;
+            prevX = (int)transform.position.x;
+            prevY = (int)transform.position.y;
+            if (moveLeft)
+            {
+                velX = -5;
+            }
+            else if (moveRight)
+            {
+                velX = 5;
+            }
+            else if (moveUp)
+            {
+                velY = 5;
+            }
+            else if (moveDown)
+            {
+                velY = -5;
+            }
+            else
+            {
+                isStill = true;
+            }
+    
         }
-        if (moveRight)
+        else
         {
-            rb.AddForce(new Vector2(5, 0), ForceMode2D.Impulse);
+            rb.AddForce(new Vector2(velX, velY), ForceMode2D.Impulse);
+            if(Math.Abs(((int)transform.position.x) - prevX) >= 10 || Math.Abs(((int)transform.position.y) - prevY) >= 10)
+            {
+                isStill = true;
+                velX = 0;
+                velY = 0;
+                rb.velocity = Vector2.zero;
+            }
         }
-        if (moveUp)
-        {
-            rb.AddForce(new Vector2(0, 5), ForceMode2D.Impulse);
-        }
-        if (moveDown)
-        {
-            rb.AddForce(new Vector2(0, -5), ForceMode2D.Impulse);
-        }
+        
     }
 }
