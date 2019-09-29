436346a7783e800024b9f01282bb1e6f94d47aa3
Shadman <shadman.hassan@live.ca>
2019-09-28 20:48:59 -0400

    preliminary grid-based movement

---
+++ Assets/Scripts/levelFinishController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class levelFinishController : MonoBehaviour
+{
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("mech"))
+        {
+            Debug.Log("Level complete");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -9,6 +9,8 @@ public class playerController : MonoBehaviour
     private Rigidbody2D rb;
     [SerializeField]
     private Transform transform;
+    [SerializeField]
+    private float speed = 1.0f;
 
     bool moveLeft = false;
     bool moveRight = false;
@@ -16,69 +18,49 @@ public class playerController : MonoBehaviour
     bool moveDown = false;
 
     private bool snapMovement;
-    private bool isStill = true;
-    private int prevX;
-    private int prevY;
+    public bool isStill = true;
+    private Vector2 pos;
     private int velX = 0;
     private int velY = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        prevX = (int)transform.position.x;
-        prevY = (int)transform.position.y;
+        pos = transform.position;
     }
 
     // Update is called once per frame
     private void Update()
     {
-        
-        moveLeft = Input.GetAxis("Horizontal") < 0;
-        moveRight = Input.GetAxis("Horizontal") > 0;
-        moveUp = Input.GetAxis("Vertical") > 0;
-        moveDown = Input.GetAxis("Vertical") < 0;
+            moveLeft = Input.GetKey(KeyCode.LeftArrow);
+            moveRight = Input.GetKey(KeyCode.RightArrow);
+            moveUp = Input.GetKey(KeyCode.UpArrow);
+            moveDown = Input.GetKey(KeyCode.DownArrow);
     }
 
     private void FixedUpdate()
     {
-        if (isStill)
+        if (transform.position.x == pos.x && transform.position.y == pos.y)
         {
-            isStill = false;
-            prevX = (int)transform.position.x;
-            prevY = (int)transform.position.y;
             if (moveLeft)
             {
-                velX = -5;
+                pos += new Vector2(-10,0);
             }
             else if (moveRight)
             {
-                velX = 5;
+                pos += new Vector2(10,0);
             }
             else if (moveUp)
             {
-                velY = 5;
+                pos += new Vector2(0,10);
             }
             else if (moveDown)
             {
-                velY = -5;
-            }
-            else
-            {
-                isStill = true;
-            }
-    
-        }
-        else
-        {
-            rb.AddForce(new Vector2(velX, velY), ForceMode2D.Impulse);
-            if(Math.Abs(((int)transform.position.x) - prevX) >= 10 || Math.Abs(((int)transform.position.y) - prevY) >= 10)
-            {
-                isStill = true;
-                velX = 0;
-                velY = 0;
-                rb.velocity = Vector2.zero;
+                pos += new Vector2(0,-10);
             }
         }
+
+        transform.position = Vector2.MoveTowards(transform.position, pos, Time.deltaTime * speed);
         
     }
 }
